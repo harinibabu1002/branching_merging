7c08045b757de927e4f418cceece048033d3df1b
harinibabu1002 <harinibabu020625@gmail.com>
2025-09-21 23:34:18 +0530

    creating new problem in master

--- multiplication.py
+++ multiplication.py
@@ -1,6 +1,6 @@
 a=int(input())
 b=int(input())
 print(a*b)
-print("branch multiplication 4")
+print("branch multiplication 6")
 print("branch multiplication commit 2")
 print("branch multiplication commit 3")
